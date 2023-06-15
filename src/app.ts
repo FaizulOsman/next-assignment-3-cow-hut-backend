6d327df0b31f60a4c507baa68d51705d350cae44
FaizulOsman <faizulosmanrabby@gmail.com>
2023-06-15 23:48:37 +0600

    feat: handle not found route setup done

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,33 @@
 import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
+import httpStatus from "http-status";
 
 const app: Application = express();
 
 // using cors
 app.use(cors());
 
+// middleware/parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("Hello World!");
+  next();
+});
+
+// Handle Not Found Route
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "Not Found",
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: "API Not Found",
+      },
+    ],
+  });
   next();
 });
 
